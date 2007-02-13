0ca354982706393837bfc2836fb7b1331e7863a0
Alexander Heavner <alex@zope.com>
2007-02-13 22:14:34 +0000

    Changed the version number in setup.py and also made it import find_packages and then used find_packages. Changed the try, except statement in __init__.py. Removed setup.cfg.in, INSTALL.txt, develop.py, CHANGES.txt, test.py, MANIFEST.in and README.txt because they were no longer needed.

--- setup.py
+++ setup.py
@@ -18,20 +18,17 @@ $Id$
 
 import os
 
-try:
-    from setuptools import setup, Extension
-except ImportError, e:
-    from distutils.core import setup, Extension
+from setuptools import setup, find_packages
 
 setup(name='zope.pagetemplate',
-      version='3.3-dev',
+      version='3.3dev',
       url='http://svn.zope.org/zope.pagetemplate',
       license='ZPL 2.1',
       description='Zope Page Templates',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       
-      packages=['zope', 'zope.pagetemplate'],
+      packages=find_packages('src'),
       package_dir = {'': 'src'},
 
       namespace_packages=['zope',],
