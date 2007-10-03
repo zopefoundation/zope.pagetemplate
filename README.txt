022a2ff699d17863f725bff32beb2b1083e909ae
Stephan Richter <stephan.richter@gmail.com>
2007-10-03 03:18:27 +0000

    Updated meta-data and get ready for the release.

---
+++ README.txt
@@ -0,0 +1,4 @@
+Page Templates provide an elegant templating mechanism that achieves a
+clean separation of presentation and application logic while allowing
+for designers to work with templates in their visual editing tools
+(FrontPage, Dreamweaver, GoLive, etc.).
--- setup.py
+++ setup.py
@@ -15,29 +15,53 @@
 
 $Id$
 """
-
 import os
-
 from setuptools import setup, find_packages
 
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
 setup(name='zope.pagetemplate',
-      version = '3.4.0b1',
-      url='http://svn.zope.org/zope.pagetemplate',
-      license='ZPL 2.1',
-      description='Zope Page Templates',
+      version = '3.4.0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
-      
+      description='Zope Page Templates',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          'Detailed Documentation' +
+          '----------------------'
+          + '\n\n' +
+          read('src', 'zope', 'pagetemplate', 'architecture.txt')
+          + '\n\n' +
+          read('src', 'zope', 'pagetemplate', 'readme.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 page template",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.pagetemplate',
+      license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
-
-      namespace_packages=['zope',],
+      namespace_packages=['zope'],
+      extras_require = dict(
+          test=['zope.testing',
+                ]),
       install_requires=['setuptools',
                         'zope.interface',
                         'zope.tales',
                         'zope.tal',
                        ],
       include_package_data = True,
-
       zip_safe = False,
       )
