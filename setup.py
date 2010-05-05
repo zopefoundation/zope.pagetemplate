0770c7e9f7a80ba110e47a6e532f30c355a7d9c3
Tres Seaver <tseaver@palladion.com>
2010-05-05 18:52:13 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006 Zope Corporation and Contributors.
+# Copyright (c) 2006 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -30,7 +30,7 @@ def read(*rnames):
 
 setup(name='zope.pagetemplate',
       version='3.5.2dev',
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Page Templates',
       long_description=(
