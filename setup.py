0552c5d9c7495ee08d75e33e36137b114c5611c2
Hanno Schlichting <hanno@hannosch.eu>
2011-08-23 11:08:33 +0000

    vb

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
 
 
 setup(name='zope.pagetemplate',
-      version='3.6.1',
+      version='3.6.2dev',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Page Templates',
