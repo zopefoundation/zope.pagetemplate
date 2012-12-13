709e04fae125df0950280b96a353b9fa9768965d
Marius Gedminas <marius@gedmin.as>
2012-12-13 15:27:11 +0000

    Preparing release 4.0.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
 
 
 setup(name='zope.pagetemplate',
-      version='4.0.0dev',
+      version='4.0.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Page Templates',
