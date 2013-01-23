53cef712e18d1cd0eb43c3229ad394211f929f3d
Tres Seaver <tseaver@palladion.com>
2013-01-23 19:53:28 +0000

    svb

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.pagetemplate',
-      version='4.0.1',
+      version='4.0.2dev',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Page Templates',
