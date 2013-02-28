9e1f4f2edf041fb5ba600c874c3365cad5246da2
Stephan Richter <stephan.richter@gmail.com>
2013-02-28 13:33:55 -0500

    Back to development: 4.0.4

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ TESTS_REQUIRE = [
 
 
 setup(name='zope.pagetemplate',
-      version='4.0.3',
+      version='4.0.4.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Page Templates',
