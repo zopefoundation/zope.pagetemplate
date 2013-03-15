a7ed2eaeca9c4ba83e39cdf9cf8940ab54d74349
Marius Gedminas <marius@gedmin.as>
2013-03-15 20:22:25 +0200

    Preparing release 4.0.4

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ TESTS_REQUIRE = [
 
 
 setup(name='zope.pagetemplate',
-      version='4.0.4.dev0',
+      version='4.0.4',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Page Templates',
