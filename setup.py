1224e11849a541f4ca0c029e2b283a7c3e9fb14b
Michael Howitz <mh@gocept.com>
2018-10-05 16:51:21 +0200

    Back to development: 4.5

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ TESTS_REQUIRE = [
 
 
 setup(name='zope.pagetemplate',
-      version='4.4',
+      version='4.5.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Page Templates',
