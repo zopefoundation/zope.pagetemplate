3a89c764da965802765337c61f9d2fca76d3a55c
Michael Howitz <mh@gocept.com>
2018-10-05 16:50:54 +0200

    Preparing release 4.4

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ TESTS_REQUIRE = [
 
 
 setup(name='zope.pagetemplate',
-      version='4.3.1.dev0',
+      version='4.4',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Page Templates',
