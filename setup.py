f0915eb8e0eb8fe2419a3021f54eed8075fa986a
Michael Howitz <icemac@gmx.net>
2026-02-18 10:00:57 +0100

    config with pure python template 5a7f2bc0 (#41)
    * Configuring for pure-python
    
    * Move package metadata from setup.py to pyproject.toml.
    
    * Move package metadata from setup.py to pyproject.toml.
    
    * Update documentation configuration to use importlib.metadata for versioning and dynamic copyright year

--- setup.py
+++ setup.py
@@ -18,77 +18,8 @@
 ##############################################################################
 """Setup for zope.pagetemplate package
 """
-import os
-
 from setuptools import setup
 
 
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
-
-
-TESTS_REQUIRE = [
-    'zope.proxy',
-    'zope.security',
-    'zope.testing',
-    'zope.testrunner >= 6.4',
-    'zope.untrustedpython >= 5.0.dev0',
-]
-
-
-setup(name='zope.pagetemplate',
-      version='6.2.dev0',
-      author='Zope Foundation and Contributors',
-      author_email='zope-dev@zope.dev',
-      description='Zope Page Templates',
-      long_description=(
-          read('README.rst')
-          + '\n\n' +
-          read('CHANGES.rst')),
-      keywords="zope3 page template",
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Zope Public License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3.11',
-          'Programming Language :: Python :: 3.12',
-          'Programming Language :: Python :: 3.13',
-          'Programming Language :: Python :: 3.14',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Topic :: Internet :: WWW/HTTP',
-          'Framework :: Zope :: 3',
-      ],
-      url='https://github.com/zopefoundation/zope.pagetemplate',
-      license='ZPL-2.1',
-      python_requires='>=3.10',
-      extras_require={
-          'test': TESTS_REQUIRE,
-          'untrusted': [
-              'zope.untrustedpython >= 5.0.dev0',
-          ],
-          'docs': [
-              'Sphinx',
-              'repoze.sphinx.autointerface',
-          ],
-      },
-      install_requires=[
-          'setuptools',
-          'zope.interface',
-          'zope.component',
-          'zope.tales',
-          'zope.tal >= 4.2.0',
-          'zope.i18n >= 4.0.1',
-          'zope.i18nmessageid',
-          'zope.traversing',
-      ],
-      include_package_data=True,
-      zip_safe=False,
-      )
+# See pyproject.toml for package metadata
+setup()
